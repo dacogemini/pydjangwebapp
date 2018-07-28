31d2ec15d12141afc7ae1e2b8f19678aef821ae7
Dan Collins <dcollins31@gmail.com>
2018-07-28 18:05:08 -0500

    Added navbar

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,10 +6,14 @@ with the newly created index.html.
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url('', views.index, name='home'),
+    url('about/', TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url('contact/', TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
     url('admin/', admin.site.urls),
 ]
